ba33a9580928d4602b6f748f150b767f67cc9e98
Anagha <anaghajayaprakash1996@gmail.com>
2024-01-09 22:58:36 -0600

    Initial Commit

---
+++ bark/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bark
+//
+//  Created by Anagha K J on 09/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bark/barkApp.swift
@@ -0,0 +1,17 @@
+//
+//  barkApp.swift
+//  bark
+//
+//  Created by Anagha K J on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct barkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
